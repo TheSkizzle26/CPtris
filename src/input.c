780935f1bd38c81365cdebc60bfec80bc1deb669
TheSkizzle26 <timobarth12@outlook.de>
2026-09-07 20:32:00 +0200

    change keymap

--- src/input.c
+++ src/input.c
@@ -32,10 +32,10 @@ void input_tick() {
     input_last.a = input_current.a;
     input_last.b = input_current.b;
 
-    input_current.up = cp_isKeyDown(CP_KEY_8);
-    input_current.down = cp_isKeyDown(CP_KEY_2);
-    input_current.left = cp_isKeyDown(CP_KEY_4);
-    input_current.right = cp_isKeyDown(CP_KEY_6);
+    input_current.up = false;
+    input_current.down = false;
+    input_current.left = cp_isKeyDown(CP_KEY_7);
+    input_current.right = cp_isKeyDown(CP_KEY_8);
     input_current.a = cp_isKeyDown(CP_KEY_EXE);
     input_current.b = cp_isKeyDown(CP_KEY_EXP);
 }
